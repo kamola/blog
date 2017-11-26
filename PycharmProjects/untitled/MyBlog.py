87fc783cbf44fbd87f754ae0d992d537561c0cc4
Kamola <kamola@nyu.edu>
2017-11-26 17:50:03 +0500

    my first commit

---
+++ PycharmProjects/untitled/MyBlog.py
@@ -0,0 +1,13 @@
+def hi(name):
+   print ('Hi'+ name + '!')
+
+girls = ['Olya','Yulya','Sonya']
+for name in girls :
+     hi (name)
+     print('next girl')
+for i in range (1,6):
+    print(i)
+
+
+
+
